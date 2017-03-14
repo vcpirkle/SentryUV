699c83a714b3c02f81d42bc309329ef6cee4ab69
Victor Pirkle <vcpirkle@gmail.com>
2017-03-14 07:43:12 -0500

    Adding include for product banner callout

---
+++ includes/iCalloutProductBanner.php
@@ -0,0 +1,14 @@
+<?php
+?>
+
+<!-- BUTTON CALLOUT -->
+<a href="#purchase" rel="nofollow" target="_blank"
+class="btn btn-xlg btn-default size-20 fullwidth nomargin noradius padding-40 noborder-left noborder-right noborder-bottom">
+
+  <span class="font-lato size-30">Interested in what SentryUV can do for you? 
+  <strong>Check out our products &#187;</strong></span>
+</a>
+<!-- /BUTTON CALLOUT -->
+
+<?php
+?>
--- index.php
+++ index.php
@@ -1,19 +1,17 @@
 <?php
-   include_once('includes/Config.php');  
-   
-   $wrapper = new Wrapper();
-   $wrapper->setTitle('SentryUV');
-   $wrapper->writeHeader();
+include_once('includes/Config.php');  
+
+$wrapper = new Wrapper();
+$wrapper->setTitle('SentryUV');
+$wrapper->writeHeader();
 ?>
- 
- <div class="container">
-	<h4>This is example of Slider Gym - Revolution slider 5</h4>
-	  <p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lorem quam, adipiscing condimentum tristique vel, eleifend sed turpis. Pellentesque cursus arcu id magna euismod in elementum purus molestie.
-		   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lorem quam, adipiscing condimentum tristique vel, eleifend sed turpis. Pellentesque cursus arcu id magna euismod in elementum purus molestie.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lorem quam, adipiscing condimentum tristique vel, eleifend sed turpis. Pellentesque cursus arcu id magna euismod in elementum purus molestie.
-	  </p>
-</div>
- 
- <?php
- $wrapper->writeFooter();
- ?>
+
+<!-- Render The Blog !-->
+
+<?php
+
+   //Include the home page slider
+   require_once('includes/iCalloutProductBanner.php');
+         
+$wrapper->writeFooter();
+?>
