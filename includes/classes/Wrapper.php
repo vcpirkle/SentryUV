0cb68c3324ebc559cd37a5d81b67704fc0602f42
Victor Pirkle <vcpirkle@gmail.com>
2017-03-15 08:40:15 -0500

    Updating wrapper class

--- includes/classes/Wrapper.php
+++ includes/classes/Wrapper.php
@@ -176,49 +176,6 @@ class Wrapper {
 							</li>
 							<!-- /SEARCH -->
 
-							<!-- QUICK SHOP CART -->
-							<li class="quick-cart">
-								<a href="#">
-									<span class="badge badge-aqua btn-xs badge-corner">2</span>
-									<i class="fa fa-shopping-cart"></i> 
-								</a>
-								<div class="quick-cart-box">
-									<h4>Shop Cart</h4>
-
-									<div class="quick-cart-wrapper">
-
-										<a href="#"><!-- cart item -->
-											<img src="assets/images/demo/people/300x300/4-min.jpg" width="45" height="45" alt="" />
-											<h6><span>2x</span> RED BAG WITH HUGE POCKETS</h6>
-											<small>$37.21</small>
-										</a><!-- /cart item -->
-
-										<a href="#"><!-- cart item -->
-											<img src="assets/images/demo/people/300x300/5-min.jpg" width="45" height="45" alt="" />
-											<h6><span>2x</span> THIS IS A VERY LONG TEXT AND WILL BE TRUNCATED</h6>
-											<small>$17.18</small>
-										</a><!-- /cart item -->
-
-										<!-- cart no items example -->
-										<!--
-										<a class="text-center" href="#">
-											<h6>0 ITEMS ON YOUR CART</h6>
-										</a>
-										-->
-
-									</div>
-
-									<!-- quick cart footer -->
-									<div class="quick-cart-footer clearfix">
-										<a href="shop-cart.html" class="btn btn-primary btn-xs pull-right">VIEW CART</a>
-										<span class="pull-left"><strong>TOTAL:</strong> $54.39</span>
-									</div>
-									<!-- /quick cart footer -->
-
-								</div>
-							</li>
-							<!-- /QUICK SHOP CART -->
-
 						</ul>
 						<!-- /BUTTONS -->
 
