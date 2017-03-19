4df0e7f190597a4d0cae6f20430df0ed7abf4e65
Victor Pirkle <vcpirkle@gmail.com>
2017-03-19 17:17:55 -0500

    Revert "adding anchor install"
    This reverts commit f669a5bfa9ffbe272dc8614d22ce2dcabdbae9b4.

--- anchor/index.php
+++ anchor/index.php
@@ -24,9 +24,9 @@
 define('DS', DIRECTORY_SEPARATOR);
 define('ENV', getenv('APP_ENV'));
 define('VERSION', '0.12.1');
-define('MIGRATION_NUMBER', 220);
+define('MIGRATION_NUMBER', 211);
 
-define('PATH', __DIR__ . DS);
+define('PATH', dirname(__FILE__) . DS);
 define('APP', PATH . 'anchor' . DS);
 define('SYS', PATH . 'system' . DS);
 define('EXT', '.php');
