87c0d947eafdce825e731970f67bb9ab86ab9fb4
Victor Pirkle <vcpirkle@gmail.com>
2017-03-24 17:03:34 -0500

    Anchor test

---
+++ anchor_test.php
@@ -0,0 +1,23 @@
+<?php
+include_once('includes/Config.php');  
+
+$wrapper = new Wrapper();
+$wrapper->setTitle('SentryUV');
+$wrapper->writeHeader();
+?>
+
+//Include Anchor
+<?php
+
+include_once('anchor/index.php');
+
+?>
+
+
+<?php
+
+   //Include the home page slider
+   require_once('includes/iCalloutProductBanner.php');
+         
+$wrapper->writeFooter();
+?>
