8167418746f58bae8c7771e979639d86fea29175
Victor Pirkle <vcpirkle@gmail.com>
2017-03-24 22:37:14 -0500

    asdf

--- anchor/themes/Plain/posts.php
+++ anchor/themes/Plain/posts.php
@@ -16,9 +16,9 @@
             <!-- POST ITEM -->
             <div class="blog-post-item article-<?php echo article_id(); ?>">
             
-            <?php $postImage1 = post_custom_field('post_image_1'); ?>
-            <?php $postImage2 = post_custom_field('post_image_2'); ?>
-            <?php $postImage3 = post_custom_field('post_image_3'); ?>
+            <?php $postImage1 = article_custom_field('post_image_1'); ?>
+            <?php $postImage2 = article_custom_field('post_image_2'); ?>
+            <?php $postImage3 = article_custom_field('post_image_3'); ?>
 
                <!-- OWL SLIDER -->
                <!--<div class="owl-carousel buttons-autohide controlls-over" data-plugin-options='{"items": 1, "autoPlay": 3000, "autoHeight": false, "navigation": true, "pagination": true, "transitionStyle":"fadeUp", "progressBar":"false"}'>
@@ -35,7 +35,7 @@
                <!-- /OWL SLIDER -->
                
                <!-- IMAGE -->
-               <?php if(!empty($postImage1) && empty($postImage2)): ?>
+               <?php if(!empty($postImage1)): ?>
                <figure class="margin-bottom-20">
                   <img class="img-responsive" src="<?php echo $postImage1; ?>" alt="<?php echo article_title(); ?>">
                </figure>
