63fba9c3e16da4e137639c2d148a275b42f41b95
Victor Pirkle <vcpirkle@gmail.com>
2017-03-24 19:40:37 -0500

    asdf

--- anchor/themes/Plain/posts.php
+++ anchor/themes/Plain/posts.php
@@ -36,7 +36,7 @@
                   <li>
                      <a href="#">
                         <i class="fa fa-clock-o"></i> 
-                        <span class="font-lato"><?php echo date(DATE_W3C, article_time()); ?>"><?php if(PLAIN_POST_RELATIVE_TIME == false): echo date('F j, Y', article_time()); else: echo relative_time(article_time()); endif; ?></span>
+                        <span class="font-lato"><?php if(PLAIN_POST_RELATIVE_TIME == false): echo date('F j, Y', article_time()); else: echo relative_time(article_time()); endif; ?></span>
                      </a>
                   </li>
                   <li>
@@ -58,7 +58,7 @@
                   <li>
                      <a href="#">
                         <i class="fa fa-user"></i> 
-                        <span class="font-lato">John Doe</span>
+                        <span class="font-lato"><?php echo article_author(); ?></span>
                      </a>
                   </li>
                </ul>
@@ -128,118 +128,7 @@
                </ul>
                <!-- /side navigation -->
 
-            
             </div>
-
-
-            <!-- JUSTIFIED TAB -->
-            <div class="tabs nomargin-top hidden-xs margin-bottom-60">
-
-               <!-- tabs -->
-               <ul class="nav nav-tabs nav-bottom-border nav-justified">
-                  <li class="active">
-                     <a href="#tab_1" data-toggle="tab">
-                        Popular
-                     </a>
-                  </li>
-                  <li>
-                     <a href="#tab_2" data-toggle="tab">
-                        Recent
-                     </a>
-                  </li>
-               </ul>
-
-               <!-- tabs content -->
-               <div class="tab-content margin-bottom-60 margin-top-30">
-
-                  <!-- POPULAR -->
-                  <div id="tab_1" class="tab-pane active">
-
-                     <div class="row tab-post"><!-- post -->
-                        <div class="col-md-3 col-sm-3 col-xs-3">
-                           <a href="blog-sidebar-left.html">
-                              <img src="assets/images/demo/people/300x300/1-min.jpg" width="50" alt="" />
-                           </a>
-                        </div>
-                        <div class="col-md-9 col-sm-9 col-xs-9">
-                           <a href="blog-sidebar-left.html" class="tab-post-link">Maecenas metus nulla</a>
-                           <small>June 29 2014</small>
-                        </div>
-                     </div><!-- /post -->
-
-                     <div class="row tab-post"><!-- post -->
-                        <div class="col-md-3 col-sm-3 col-xs-3">
-                           <a href="blog-sidebar-left.html">
-                              <img src="assets/images/demo/people/300x300/2-min.jpg" width="50" alt="" />
-                           </a>
-                        </div>
-                        <div class="col-md-9 col-sm-9 col-xs-9">
-                           <a href="blog-sidebar-left.html" class="tab-post-link">Curabitur pellentesque neque eget diam</a>
-                           <small>June 29 2014</small>
-                        </div>
-                     </div><!-- /post -->
-
-                     <div class="row tab-post"><!-- post -->
-                        <div class="col-md-3 col-sm-3 col-xs-3">
-                           <a href="blog-sidebar-left.html">
-                              <img src="assets/images/demo/people/300x300/3-min.jpg" width="50" alt="" />
-                           </a>
-                        </div>
-                        <div class="col-md-9 col-sm-9 col-xs-9">
-                           <a href="blog-sidebar-left.html" class="tab-post-link">Nam et lacus neque. Ut enim massa, sodales</a>
-                           <small>June 29 2014</small>
-                        </div>
-                     </div><!-- /post -->
-
-                  </div>
-                  <!-- /POPULAR -->
-
-
-                  <!-- RECENT -->
-                  <div id="tab_2" class="tab-pane">
-
-                     <div class="row tab-post"><!-- post -->
-                        <div class="col-md-3 col-sm-3 col-xs-3">
-                           <a href="blog-sidebar-left.html">
-                              <img src="assets/images/demo/people/300x300/4-min.jpg" width="50" alt="" />
-                           </a>
-                        </div>
-                        <div class="col-md-9 col-sm-9 col-xs-9">
-                           <a href="blog-sidebar-left.html" class="tab-post-link">Curabitur pellentesque neque eget diam</a>
-                           <small>June 29 2014</small>
-                        </div>
-                     </div><!-- /post -->
-
-                     <div class="row tab-post"><!-- post -->
-                        <div class="col-md-3 col-sm-3 col-xs-3">
-                           <a href="blog-sidebar-left.html">
-                              <img src="assets/images/demo/people/300x300/5-min.jpg" width="50" alt="" />
-                           </a>
-                        </div>
-                        <div class="col-md-9 col-sm-9 col-xs-9">
-                           <a href="blog-sidebar-left.html" class="tab-post-link">Maecenas metus nulla</a>
-                           <small>June 29 2014</small>
-                        </div>
-                     </div><!-- /post -->
-
-                     <div class="row tab-post"><!-- post -->
-                        <div class="col-md-3 col-sm-3 col-xs-3">
-                           <a href="blog-sidebar-left.html">
-                              <img src="assets/images/demo/people/300x300/6-min.jpg" width="50" alt="" />
-                           </a>
-                        </div>
-                        <div class="col-md-9 col-sm-9 col-xs-9">
-                           <a href="blog-sidebar-left.html" class="tab-post-link">Quisque ut nulla at nunc</a>
-                           <small>June 29 2014</small>
-                        </div>
-                     </div><!-- /post -->
-                  </div>
-                  <!-- /RECENT -->
-
-               </div>
-
-            </div>
-            <!-- JUSTIFIED TAB -->
 
 
             <!-- TAGS -->
@@ -271,30 +160,6 @@
                   <span class="num">3</span>
                </a>
             </div>
-
-
-            <!-- TWIITER WIDGET -->
-            <h3 class="hidden-xs size-16 margin-bottom-10">TWITTER FEED</h3>							
-            <ul class="hidden-xs widget-twitter margin-bottom-60" data-php="php/twitter/tweet.php" data-username="stepofweb" data-limit="3">
-               <li></li>
-            </ul>
-
-            <!-- FEATURED VIDEO -->
-            <h3 class="hidden-xs size-16 margin-bottom-10">FEATURED VIDEO</h3>
-            <div class="hidden-xs embed-responsive embed-responsive-16by9 margin-bottom-60">
-               <iframe class="embed-responsive-item" src="http://player.vimeo.com/video/8408210" width="800" height="450"></iframe>
-            </div>
-
-            <!-- FLICKR WIDGET -->
-            <h3 class="hidden-xs size-16 margin-bottom-10">FLICKR PHOTO</h3>
-            <div class="hidden-xs widget-flickr clearfix lightbox margin-bottom-60" data-id="37304598@N02" data-limit="16" data-plugin-options='{"delegate": "a", "gallery": {"enabled": true}}'></div>
-
-
-            <!-- FACEBOOK -->
-            <iframe class="hidden-xs" src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fstepofweb&amp;width=263&amp;height=258&amp;colorscheme=light&amp;show_faces=true&amp;header=false&amp;stream=false&amp;show_border=false" style="border:none; overflow:hidden; width:263px; height:258px;"></iframe>
-
-
-            <hr />
 
 
             <!-- SOCIAL ICONS -->
