877eeea18b9deb00047f3c4fd5d1ee4e5166c6a7
Victor Pirkle <vcpirkle@gmail.com>
2017-03-26 16:05:44 -0500

    Checking in things I missed in anchor

---
+++ anchor/anchor/config/app.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	'url' => '/sentryuv/anchor',
+	'index' => '',
+	'timezone' => 'Pacific/Midway',
+	'key' => 'CtBgGtOLqBILNur67nZylPYRe7y3xXTg',
+	'language' => 'en_GB',
+	'encoding' => 'UTF-8'
+);
---
+++ anchor/anchor/config/db.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+	'default' => 'mysql',
+	'prefix' => 'anchor_',
+	'connections' => array(
+		'mysql' => array(
+			'driver' => 'mysql',
+			'hostname' => 'victor1.cvpcenpy8fdw.us-east-1.rds.amazonaws.com',
+			'port' => '3306',
+			'username' => 'ultraviolet_dev',
+			'password' => 'kHerijfKJ7)(42!hMJH',
+			'database' => 'SentryUV_Dev',
+			'charset' => 'utf8'
+		)
+	)
+);
