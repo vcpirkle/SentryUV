172e323772452c1fd3360742e0ebb7750e421ad6
Victor Pirkle <vcpirkle@gmail.com>
2017-05-25 06:08:15 -0500

    Testing out the first product on the page

--- products/index.php
+++ products/index.php
@@ -50,11 +50,9 @@
 
 				<ul class="nav nav-pills mix-filter margin-bottom-60">
 					<li data-filter="all" class="filter active"><a href="#">All</a></li>
-					<li data-filter="development" class="filter"><a href="#">Development</a></li>
-					<li data-filter="photography" class="filter"><a href="#">Photography</a></li>
-					<li data-filter="design" class="filter"><a href="#">Design</a></li>
+					<li data-filter="pvc" class="filter"><a href="#">PVC</a></li>
+					<li data-filter="uv" class="filter"><a href="#">UV</a></li>
 				</ul>
-
 
 				<div class="mix-grid">
 
@@ -68,7 +66,7 @@
 										<span class="inner">
 
 											<!-- lightbox -->
-											<a class="ico-rounded lightbox" href="assets/images/demo/mockups/1200x800/6-min.jpg" data-plugin-options='{"type":"image"}'>
+											<a class="ico-rounded lightbox" href="../assets/images/products/aqua-guard-pvc-120-watt.png" data-plugin-options='{"type":"image"}'>
 												<span class="fa fa-plus size-20"></span>
 											</a>
 
@@ -83,13 +81,13 @@
 									<!-- carousel -->
 									<div class="owl-carousel buttons-autohide controlls-over nomargin" data-plugin-options='{"singleItem": true, "autoPlay": 4000, "navigation": false, "pagination": true, "transitionStyle":"goDown"}'>
 										<div>
-											<img class="img-responsive" src="assets/images/demo/mockups/600x399/8-min.jpg" width="800" height="533" alt="">
+											<img class="img-responsive" src="../assets/images/products/aqua-guard-pvc-120-watt.png" width="800" height="533" alt="">
 										</div>
 										<div>
-											<img class="img-responsive" src="assets/images/demo/mockups/600x399/9-min.jpg" width="800" height="533" alt="">
+											<img class="img-responsive" src="../assets/images/products/aqua-guard-pvc-120-watt.png" width="800" height="533" alt="">
 										</div>
 										<div>
-											<img class="img-responsive" src="assets/images/demo/mockups/600x399/10-min.jpg" width="800" height="533" alt="">
+											<img class="img-responsive" src="../assets/images/products/aqua-guard-pvc-120-watt.png" width="800" height="533" alt="">
 										</div>
 									</div>
 									<!-- /carousel -->
@@ -103,26 +101,30 @@
 								<a href="portfolio-single-slider.html">Nature Photography</a>
 							</h2>
 							<ul class="list-inline categories nomargin">
-								<li><a href="#">Development</a></li>
-								<li><a href="#">Design</a></li>
+								<li><a href="#">SentryUV Aqua Guard&trade;</a></li>
+								<li><a href="#">PVC</a></li>
 							</ul>
 
 							<div class="margin-top-30"><!-- description -->
 
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas metus nulla, commodo a sodales sed, dignissim pretium nunc. Nam et lacus neque. Ut enim massa, sodales tempor convallis et, iaculis ac massa. Maecenas metus nulla, commodo a sodales sed, dignissim pretium nunc. Nam et lacus neque.</p>
+								<p>Designed to treat swimming pool and spa's and many other applications with flows of up to 100 gallons per minute. The SAG120-APVC is your answer to cryptosporidium control and chloramines destruction in commercial swimming pools and spa's and other applications.</p>
 
 								<ul class="list-unstyled">
 									<li>
 										<i class="fa fa-check"></i> 
-										<strong>Technologies:</strong> PHP, Mysql, Redis
+										<strong>Technologies:</strong> low pressure high output amalgam lamp technology
 									</li>
 									<li>
 										<i class="fa fa-check"></i> 
-										<strong>Date:</strong> 29th June, 2015
+										<strong>Technical Specifications:</strong> 120 Watts
 									</li>
 									<li>
 										<i class="fa fa-check"></i> 
-										<strong>Client:</strong> Lorem Company LLC
+										<strong>Liner:</strong> 316-L stainless steel to give reflection and to protect the PVC
+									</li>
+									<li>
+										<i class="fa fa-check"></i> 
+										<strong>Flow Rate:</strong> (gpm) ~100 gallons per minute
 									</li>
 								</ul>
 
@@ -240,444 +242,6 @@
 									<li>
 										<i class="fa fa-check"></i> 
 										<strong>Date:</strong> 29th June, 2015
-									</li>
-								</ul>
-
-								<a class="btn btn-lg btn-default btn-bordered size-15" href="portfolio-single-slider.html">PROJECT DETAIL</a>
-
-							</div>
-
-						</div><!-- /description -->
-
-					</div><!-- /item -->
-
-
-					<div class="row mix design"><!-- item -->
-
-						<div class="col-md-7"><!-- image -->
-							<div class="item-box">
-								<figure>
-									<span class="item-hover">
-										<span class="overlay dark-5"></span>
-										<span class="inner">
-
-											<!-- lightbox -->
-											<a class="ico-rounded lightbox" href="assets/images/demo/mockups/1200x800/9-min.jpg" data-plugin-options='{"type":"image"}'>
-												<span class="fa fa-plus size-20"></span>
-											</a>
-
-											<!-- details -->
-											<a class="ico-rounded" href="portfolio-single-slider.html">
-												<span class="glyphicon glyphicon-option-horizontal size-20"></span>
-											</a>
-
-										</span>
-									</span>
-									<img class="img-responsive" src="assets/images/demo/mockups/800x553/9-min.jpg" width="800" height="533" alt="">
-								</figure>
-							</div>
-						</div><!-- /image -->
-						
-						<div class="col-md-5"><!-- description -->
-							<h2>
-								<a href="portfolio-single-slider.html">Animal Study</a>
-							</h2>
-							<ul class="list-inline categories nomargin">
-								<li><a href="#">Photography</a></li>
-							</ul>
-
-							<div class="margin-top-30"><!-- description -->
-
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas metus nulla, commodo a sodales sed, dignissim pretium nunc. Nam et lacus neque. Ut enim massa, sodales tempor convallis et, iaculis ac massa. Maecenas metus nulla, commodo a sodales sed, dignissim pretium nunc. Nam et lacus neque.</p>
-
-								<ul class="list-unstyled">
-									<li>
-										<i class="fa fa-check"></i> 
-										<strong>Technologies:</strong> PHP, Mysql, Redis
-									</li>
-									<li>
-										<i class="fa fa-check"></i> 
-										<strong>Date:</strong> 29th June, 2015
-									</li>
-									<li>
-										<i class="fa fa-check"></i> 
-										<strong>Client:</strong> Lorem Company LLC
-									</li>
-								</ul>
-
-								<a class="btn btn-lg btn-default btn-bordered size-15" href="portfolio-single-slider.html">PROJECT DETAIL</a>
-
-							</div>
-
-						</div><!-- /description -->
-
-					</div><!-- /item -->
-
-
-					<div class="row mix photography"><!-- item -->
-
-						<div class="col-md-7"><!-- image -->
-							<div class="item-box">
-								<figure>
-									<span class="item-hover">
-										<span class="overlay dark-5"></span>
-										<span class="inner">
-
-											<!-- lightbox -->
-											<a class="ico-rounded lightbox" href="assets/images/demo/mockups/1200x800/10-min.jpg" data-plugin-options='{"type":"image"}'>
-												<span class="fa fa-plus size-20"></span>
-											</a>
-
-											<!-- details -->
-											<a class="ico-rounded" href="portfolio-single-slider.html">
-												<span class="glyphicon glyphicon-option-horizontal size-20"></span>
-											</a>
-
-										</span>
-									</span>
-									<img class="img-responsive" src="assets/images/demo/mockups/800x553/10-min.jpg" width="800" height="533" alt="">
-								</figure>
-							</div>
-						</div><!-- /image -->
-						
-						<div class="col-md-5"><!-- description -->
-							<h2>
-								<a href="portfolio-single-slider.html">Fashion Session</a>
-							</h2>
-							<ul class="list-inline categories nomargin">
-								<li><a href="#">Photography</a></li>
-								<li><a href="#">Design</a></li>
-							</ul>
-
-							<div class="margin-top-30"><!-- description -->
-
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas metus nulla, commodo a sodales sed, dignissim pretium nunc. Nam et lacus neque. Ut enim massa, sodales tempor convallis et, iaculis ac massa. Maecenas metus nulla, commodo a sodales sed, dignissim pretium nunc. Nam et lacus neque.</p>
-
-								<ul class="list-unstyled">
-									<li>
-										<i class="fa fa-check"></i> 
-										<strong>Technologies:</strong> PHP, Mysql, Redis
-									</li>
-									<li>
-										<i class="fa fa-check"></i> 
-										<strong>Date:</strong> 29th June, 2015
-									</li>
-									<li>
-										<i class="fa fa-check"></i> 
-										<strong>Client:</strong> Lorem Company LLC
-									</li>
-								</ul>
-
-								<a class="btn btn-lg btn-default btn-bordered size-15" href="portfolio-single-slider.html">PROJECT DETAIL</a>
-
-							</div>
-
-						</div><!-- /description -->
-
-					</div><!-- /item -->
-
-
-					<div class="row mix development"><!-- item -->
-
-						<div class="col-md-7"><!-- image -->
-							<div class="item-box">
-								<figure>
-									<span class="item-hover">
-										<span class="overlay dark-5"></span>
-										<span class="inner">
-
-											<!-- lightbox -->
-											<a class="ico-rounded lightbox" href="assets/images/demo/mockups/1200x800/11-min.jpg" data-plugin-options='{"type":"image"}'>
-												<span class="fa fa-plus size-20"></span>
-											</a>
-
-											<!-- details -->
-											<a class="ico-rounded" href="portfolio-single-slider.html">
-												<span class="glyphicon glyphicon-option-horizontal size-20"></span>
-											</a>
-
-										</span>
-									</span>
-									<img class="img-responsive" src="assets/images/demo/mockups/800x553/11-min.jpg" width="800" height="533" alt="">
-								</figure>
-							</div>
-						</div><!-- /image -->
-						
-						<div class="col-md-5"><!-- description -->
-							<h2>
-								<a href="portfolio-single-slider.html">Outdoor Photography</a>
-							</h2>
-							<ul class="list-inline categories nomargin">
-								<li><a href="#">Photography</a></li>
-								<li><a href="#">Design</a></li>
-							</ul>
-
-							<div class="margin-top-30"><!-- description -->
-
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas metus nulla, commodo a sodales sed, dignissim pretium nunc. Nam et lacus neque. Ut enim massa, sodales tempor convallis et, iaculis ac massa. Maecenas metus nulla, commodo a sodales sed, dignissim pretium nunc. Nam et lacus neque.</p>
-
-								<ul class="list-unstyled">
-									<li>
-										<i class="fa fa-check"></i> 
-										<strong>Technologies:</strong> PHP, Mysql, Redis
-									</li>
-									<li>
-										<i class="fa fa-check"></i> 
-										<strong>Date:</strong> 29th June, 2015
-									</li>
-									<li>
-										<i class="fa fa-check"></i> 
-										<strong>Client:</strong> Lorem Company LLC
-									</li>
-								</ul>
-
-								<a class="btn btn-lg btn-default btn-bordered size-15" href="portfolio-single-slider.html">PROJECT DETAIL</a>
-
-							</div>
-
-						</div><!-- /description -->
-
-					</div><!-- /item -->
-
-
-					<div class="row mix design"><!-- item -->
-
-						<div class="col-md-7"><!-- image -->
-							<div class="item-box">
-								<figure>
-									<span class="item-hover">
-										<span class="overlay dark-5"></span>
-										<span class="inner">
-
-											<!-- lightbox -->
-											<a class="ico-rounded lightbox" href="assets/images/demo/mockups/1200x800/12-min.jpg" data-plugin-options='{"type":"image"}'>
-												<span class="fa fa-plus size-20"></span>
-											</a>
-
-											<!-- details -->
-											<a class="ico-rounded" href="portfolio-single-slider.html">
-												<span class="glyphicon glyphicon-option-horizontal size-20"></span>
-											</a>
-
-										</span>
-									</span>
-									<img class="img-responsive" src="assets/images/demo/mockups/800x553/12-min.jpg" width="800" height="533" alt="">
-								</figure>
-							</div>
-						</div><!-- /image -->
-						
-						<div class="col-md-5"><!-- description -->
-							<h2>
-								<a href="portfolio-single-slider.html">Furniture Design</a>
-							</h2>
-							<ul class="list-inline categories nomargin">
-								<li><a href="#">Design</a></li>
-							</ul>
-
-							<div class="margin-top-30"><!-- description -->
-
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas metus nulla, commodo a sodales sed, dignissim pretium nunc. Nam et lacus neque. Ut enim massa, sodales tempor convallis et, iaculis ac massa. Maecenas metus nulla, commodo a sodales sed, dignissim pretium nunc. Nam et lacus neque.</p>
-
-								<ul class="list-unstyled">
-									<li>
-										<i class="fa fa-check"></i> 
-										<strong>Technologies:</strong> PHP, Mysql, Redis
-									</li>
-									<li>
-										<i class="fa fa-check"></i> 
-										<strong>Date:</strong> 29th June, 2015
-									</li>
-									<li>
-										<i class="fa fa-check"></i> 
-										<strong>Client:</strong> Lorem Company LLC
-									</li>
-								</ul>
-
-								<a class="btn btn-lg btn-default btn-bordered size-15" href="portfolio-single-slider.html">PROJECT DETAIL</a>
-
-							</div>
-
-						</div><!-- /description -->
-
-					</div><!-- /item -->
-
-
-					<div class="row mix photography"><!-- item -->
-
-						<div class="col-md-7"><!-- image -->
-							<div class="item-box">
-								<figure>
-									<span class="item-hover">
-										<span class="overlay dark-5"></span>
-										<span class="inner">
-
-											<!-- lightbox -->
-											<a class="ico-rounded lightbox" href="assets/images/demo/mockups/1200x800/13-min.jpg" data-plugin-options='{"type":"image"}'>
-												<span class="fa fa-plus size-20"></span>
-											</a>
-
-											<!-- details -->
-											<a class="ico-rounded" href="portfolio-single-slider.html">
-												<span class="glyphicon glyphicon-option-horizontal size-20"></span>
-											</a>
-
-										</span>
-									</span>
-									<img class="img-responsive" src="assets/images/demo/mockups/800x553/13-min.jpg" width="800" height="533" alt="">
-								</figure>
-							</div>
-						</div><!-- /image -->
-						
-						<div class="col-md-5"><!-- description -->
-							<h2>
-								<a href="portfolio-single-slider.html">Futuristic Speakers</a>
-							</h2>
-							<ul class="list-inline categories nomargin">
-								<li><a href="#">Design</a></li>
-							</ul>
-
-							<div class="margin-top-30"><!-- description -->
-
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas metus nulla, commodo a sodales sed, dignissim pretium nunc. Nam et lacus neque. Ut enim massa, sodales tempor convallis et, iaculis ac massa. Maecenas metus nulla, commodo a sodales sed, dignissim pretium nunc. Nam et lacus neque.</p>
-
-								<ul class="list-unstyled">
-									<li>
-										<i class="fa fa-check"></i> 
-										<strong>Technologies:</strong> PHP, Mysql, Redis
-									</li>
-									<li>
-										<i class="fa fa-check"></i> 
-										<strong>Date:</strong> 29th June, 2015
-									</li>
-									<li>
-										<i class="fa fa-check"></i> 
-										<strong>Client:</strong> Lorem Company LLC
-									</li>
-								</ul>
-
-								<a class="btn btn-lg btn-default btn-bordered size-15" href="portfolio-single-slider.html">PROJECT DETAIL</a>
-
-							</div>
-
-						</div><!-- /description -->
-
-					</div><!-- /item -->
-
-
-					<div class="row mix design"><!-- item -->
-
-						<div class="col-md-7"><!-- image -->
-							<div class="item-box">
-								<figure>
-									<span class="item-hover">
-										<span class="overlay dark-5"></span>
-										<span class="inner">
-
-											<!-- lightbox -->
-											<a class="ico-rounded lightbox" href="assets/images/demo/mockups/1200x800/14-min.jpg" data-plugin-options='{"type":"image"}'>
-												<span class="fa fa-plus size-20"></span>
-											</a>
-
-											<!-- details -->
-											<a class="ico-rounded" href="portfolio-single-slider.html">
-												<span class="glyphicon glyphicon-option-horizontal size-20"></span>
-											</a>
-
-										</span>
-									</span>
-									<img class="img-responsive" src="assets/images/demo/mockups/800x553/14-min.jpg" width="800" height="533" alt="">
-								</figure>
-							</div>
-						</div><!-- /image -->
-						
-						<div class="col-md-5"><!-- description -->
-							<h2>
-								<a href="portfolio-single-slider.html">Furniture Design</a>
-							</h2>
-							<ul class="list-inline categories nomargin">
-								<li><a href="#">Development</a></li>
-								<li><a href="#">Design</a></li>
-							</ul>
-
-							<div class="margin-top-30"><!-- description -->
-
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas metus nulla, commodo a sodales sed, dignissim pretium nunc. Nam et lacus neque. Ut enim massa, sodales tempor convallis et, iaculis ac massa. Maecenas metus nulla, commodo a sodales sed, dignissim pretium nunc. Nam et lacus neque.</p>
-
-								<ul class="list-unstyled">
-									<li>
-										<i class="fa fa-check"></i> 
-										<strong>Technologies:</strong> PHP, Mysql, Redis
-									</li>
-									<li>
-										<i class="fa fa-check"></i> 
-										<strong>Date:</strong> 29th June, 2015
-									</li>
-									<li>
-										<i class="fa fa-check"></i> 
-										<strong>Client:</strong> Lorem Company LLC
-									</li>
-								</ul>
-
-								<a class="btn btn-lg btn-default btn-bordered size-15" href="portfolio-single-slider.html">PROJECT DETAIL</a>
-
-							</div>
-
-						</div><!-- /description -->
-
-					</div><!-- /item -->
-
-
-					<div class="row mix photography"><!-- item -->
-
-						<div class="col-md-7"><!-- image -->
-							<div class="item-box">
-								<figure>
-									<span class="item-hover">
-										<span class="overlay dark-5"></span>
-										<span class="inner">
-
-											<!-- lightbox -->
-											<a class="ico-rounded lightbox" href="assets/images/demo/mockups/1200x800/15-min.jpg" data-plugin-options='{"type":"image"}'>
-												<span class="fa fa-plus size-20"></span>
-											</a>
-
-											<!-- details -->
-											<a class="ico-rounded" href="portfolio-single-slider.html">
-												<span class="glyphicon glyphicon-option-horizontal size-20"></span>
-											</a>
-
-										</span>
-									</span>
-									<img class="img-responsive" src="assets/images/demo/mockups/800x553/15-min.jpg" width="800" height="533" alt="">
-								</figure>
-							</div>
-						</div><!-- /image -->
-						
-						<div class="col-md-5"><!-- description -->
-							<h2>
-								<a href="portfolio-single-slider.html">Wood Photography</a>
-							</h2>
-							<ul class="list-inline categories nomargin">
-								<li><a href="#">Photography</a></li>
-								<li><a href="#">Design</a></li>
-							</ul>
-
-							<div class="margin-top-30"><!-- description -->
-
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas metus nulla, commodo a sodales sed, dignissim pretium nunc. Nam et lacus neque. Ut enim massa, sodales tempor convallis et, iaculis ac massa. Maecenas metus nulla, commodo a sodales sed, dignissim pretium nunc. Nam et lacus neque.</p>
-
-								<ul class="list-unstyled">
-									<li>
-										<i class="fa fa-check"></i> 
-										<strong>Technologies:</strong> PHP, Mysql, Redis
-									</li>
-									<li>
-										<i class="fa fa-check"></i> 
-										<strong>Date:</strong> 29th June, 2015
-									</li>
-									<li>
-										<i class="fa fa-check"></i> 
-										<strong>Client:</strong> Lorem Company LLC
 									</li>
 								</ul>
 
